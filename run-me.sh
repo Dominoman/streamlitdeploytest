f894dc880c71f909b794ed44c6e0fc8d7adbb815
Dominoman <olaca@freemail.hu>
2024-10-13 16:10:49 +0200

    Initial commit

---
+++ run-me.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+set -ex
+
+# Virtuális környezet létrehozása
+if [ ! -f pyvenv.cfg ] ; then
+  python -m venv .
+fi
+
+# Aktiváljuk a virtuális környezetet
+source bin/activate
+
+# Csomagok installálása
+if [ -f requirements.txt ] ; then
+  pip install -r requirements.txt
+fi
