71ff88ed1c940752e7630e592f1b3f70198932d0
Dominoman <olaca@freemail.hu>
2024-10-14 09:47:06 +0200

    Streamlit config

--- run-me.sh
+++ run-me.sh
@@ -15,3 +15,25 @@ source bin/activate
 if [ -f requirements.txt ] ; then
   pip install -r requirements.txt
 fi
+
+# streamlit config
+if [ ! -d .streamlit ] ; then
+  mkdir .streamlit
+fi
+
+if [ ! -f .streamlit/config.toml ] ; then
+  streamlit config show > .streamlit/config.toml
+fi
+
+currentpath=$(pwd)
+servicename="streamlit.service"
+if [ ! -f $servicename ] ; then
+  cp $servicename.template $servicename
+fi
+
+sed -i "s|%currentpath%|$currentpath|g" $servicename
+
+if [ ! -f /etc/systemd/system/$servicename ] ; then
+  print "xxx"
+  # sudo ln -s "$currentpath/$servicename" /etc/systemd/system/$servicename
+fi
