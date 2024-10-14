d2a83c31b135798389b5e4c32a558c1003712e57
Dominoman <olaca@freemail.hu>
2024-10-14 11:24:01 +0200

    Username/Groupname

--- run-me.sh
+++ run-me.sh
@@ -34,7 +34,7 @@ if [ ! -f $servicename ] ; then
   cp $servicename.service.template $servicename.service
 fi
 
-sed -i "s|%currentpath%|$currentpath|g s|%username%|$username|g s|%groupname%|$groupname|g" $servicename.service
+sed -i "s|%currentpath%|$currentpath|g; s|%username%|$username|g; s|%groupname%|$groupname|g" $servicename.service
 
 if [ ! -f /etc/systemd/system/$servicename.service ] ; then
   sudo ln -s "$currentpath/$servicename.service" /etc/systemd/system/$servicename.service
