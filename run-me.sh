5ad090d8fc3399f07069d9b48b25c6d74c5bba45
Dominoman <olaca@freemail.hu>
2024-10-14 11:20:22 +0200

    Username/Groupname

--- run-me.sh
+++ run-me.sh
@@ -26,12 +26,15 @@ if [ ! -f .streamlit/config.toml ] ; then
 fi
 
 currentpath=$(pwd)
+username=$(whoami)
 servicename="streamlit"
+groupname=$(id -gn)
+
 if [ ! -f $servicename ] ; then
   cp $servicename.service.template $servicename.service
 fi
 
-sed -i "s|%currentpath%|$currentpath|g" $servicename.service
+sed -i "s|%currentpath%|$currentpath|g s|%username%|$username|g s|%groupname%|$groupname|g" $servicename.service
 
 if [ ! -f /etc/systemd/system/$servicename.service ] ; then
   sudo ln -s "$currentpath/$servicename.service" /etc/systemd/system/$servicename.service
